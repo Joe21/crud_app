806daa85ffcbba114f04122d256236bd1415df6d
Joe21 <joej21@gmail.com>
2013-10-29 08:48:29 -0400

    fixed stores controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,35 @@
+class StoresController < ApplicationController
+
+	def index
+		@stores = Store.all
+	end
+
+	def new
+		@store = Store.new
+	end
+
+	def create
+		store = Store.create(params[:store])
+		redirect_to root_path
+	end
+
+	def show
+		id = params[:id]
+		@store = Store.find(id)
+	end
+
+	def update
+		id = params[:id]
+		store = Store.find(id)
+		store.update_attributes(params[:store])
+		store.save
+		redirect_to root_path
+	end
+
+	def destroy
+		id = params[:id]
+		store = Store.find(id)
+		store.destroy
+		redirect_to root_path
+	end
+end
